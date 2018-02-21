980d16d867635ba8c98df20438ec81179470ee44
nelsonic <contact.nelsonic+github@gmail.com>
2018-02-21 09:46:38 +0000

    fix syntax error in config/prod.exs (missing comma!)

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :chat, ChatWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "phxchat.herokuapp.com",
-    port: 443]
+    port: 443],
     # port: System.get_env("PORT")], # Heroku Supplies the TCP Port
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
