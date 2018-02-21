a8faa4bbd82a06675eb9bf409659e76db220f3e1
nelsonic <contact.nelsonic+github@gmail.com>
2018-02-21 11:37:56 +0000

    define minimum version of postgrex in mix.exs for #21

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Chat.Mixfile do
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, ">= 0.13.5"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
