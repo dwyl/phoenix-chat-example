62dbb9d0d52187927ea1f3c0b4cbba95407e75fc
nelsonic <contact.nelsonic+github@gmail.com>
2018-04-28 13:47:58 +0100

    simplify init function in lib/chat_web/endpoint.ex as we are not using :load_from_system_env for now #7

--- lib/chat_web/endpoint.ex
+++ lib/chat_web/endpoint.ex
@@ -46,12 +46,12 @@ defmodule ChatWeb.Endpoint do
   It receives the endpoint configuration and checks if
   configuration should be loaded from the system environment.
   """
-  def init(_key, config) do
-    if config[:load_from_system_env] do
-      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
-    else
-      {:ok, config}
-    end
+  def init(_key, config) do # not using :load_from_system_env for now.
+    # if config[:load_from_system_env] do
+    #   # port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+    #   {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+    # else
+    {:ok, config}
+    # end
   end
 end
