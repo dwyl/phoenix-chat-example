6966b1dc56b440fa0834df76830a49d9e162fee8
nelsonic <nelson+github@dwyl.io>
2018-11-16 04:16:04 +0000

    make necessary config changes from phx 1.3 to 1.4 see: https://github.com/dwyl/phoenix-chat-example/pull/13

--- config/config.exs
+++ config/config.exs
@@ -3,15 +3,17 @@
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
-use Mix.Config
 
 # General application configuration
-config :chat, ecto_repos: [Chat.Repo]
+use Mix.Config
+
+config :chat,
+  ecto_repos: [Chat.Repo]
 
 # Configures the endpoint
 config :chat, ChatWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "iH1ZNacvXr4Q9Qe7zn1s8HeDc6Dw1Oz0jKAhYGjenFqm7jAJhoXHzjbRypftyTOW",
+  secret_key_base: "B1/lHcRWBn1I9WenGINix4hEWcrI3TIk5a1SL1tRzDXqY/vSR5n9RcQYzwpcshkE",
   render_errors: [view: ChatWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Chat.PubSub, adapter: Phoenix.PubSub.PG2]
 
@@ -19,6 +21,9 @@ config :chat, ChatWeb.Endpoint,
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
