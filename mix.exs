ff1aaf64e0e7acaeed26d552428b2214472385ac
nelsonic <nelson+github@dwyl.io>
2019-09-01 10:29:19 +0100

    downgrade version of elixir in mix.exs to 1.5 to avoid build failing on Heroku see: https://github.com/dwyl/phoenix-chat-example/issues/27

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chat.Mixfile do
     [
       app: :chat,
       version: "1.4.4",
-      elixir: "~> 1.7",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
