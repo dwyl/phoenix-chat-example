c26db1bf8e2fea250b5b1f9cd263aa8c35f70175
Sreenadh TC <kesav.tc8@gmail.com>
2019-10-12 16:46:15 +0530

    Add to supervisor tree

--- lib/chat/application.ex
+++ lib/chat/application.ex
@@ -11,9 +11,10 @@ defmodule Chat.Application do
       # Start the Ecto repository
       supervisor(Chat.Repo, []),
       # Start the endpoint when the application starts
-      supervisor(ChatWeb.Endpoint, [])
+      supervisor(ChatWeb.Endpoint, []),
       # Start your own worker by calling: Chat.Worker.start_link(arg1, arg2, arg3)
       # worker(Chat.Worker, [arg1, arg2, arg3]),
+      Chat.Presence
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
