750be3fa836cf8b4589add2597c1d88debc1a9dc
nelsonic <nelson+github@dwyl.io>
2020-05-20 22:26:50 +0100

    Step 2: Create Channel #38

--- lib/chat_web/channels/user_socket.ex
+++ lib/chat_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChatWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ChatWeb.RoomChannel
+  channel "room:lobby", ChatWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
