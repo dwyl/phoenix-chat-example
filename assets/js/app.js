eb4bd835d28b7cfbc08f3f88bd333bfafaba1e7d
nelsonic <nelson+github@dwyl.com>
2021-10-15 11:14:08 +0100

    rename phoenix.scss to phoenix.css to avoid needing a pre-processor #61

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/phoenix.css"
+import "../css/app.css"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
