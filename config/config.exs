ec38280a6056a41076137b9b38589c4873975fdf
SimonLab <simon.labondance@gmail.com>
2021-10-20 10:31:51 +0100

    remove swoosh

--- config/config.exs
+++ config/config.exs
@@ -26,10 +26,6 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Swoosh API client is needed for adapters other than SMTP.
-config :swoosh, :api_client, false
-
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- mix.exs
+++ mix.exs
@@ -50,7 +50,6 @@ defmodule Chat.Mixfile do
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.5"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18.2"},
