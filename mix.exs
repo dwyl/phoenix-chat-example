dbb9de2867766a8f63299ad9ebcf5a065320cfb9
SimonLab <simon.labondance@gmail.com>
2021-10-20 10:04:55 +0100

    clean static folder

--- mix.exs
+++ mix.exs
@@ -82,7 +82,7 @@ defmodule Chat.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
+      setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
