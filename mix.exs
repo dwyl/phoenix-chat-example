63f3acec3cec8b3d959d347dda9f7d53821c0d19
Luís Arteiro <lucho14garfield@gmail.com>
2022-10-12 14:13:51 +0100

    feat: Updating deps for deployment.

--- mix.exs
+++ mix.exs
@@ -48,7 +48,21 @@ defmodule Chat.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      # Wake Heroku App. See: https://github.com/dwyl/ping
+      {:ping, "~> 1.1.0"},
+
+      # sanitise data to avoid XSS see: https://git.io/fjpGZ
+      {:html_sanitize_ex, "~> 1.4"},
+
+      # The rest of the dependendencies are for testing/reporting
+      # tracking test coverage
+      {:excoveralls, "~> 0.15.0", only: [:test, :dev]},
+      # documentation
+      {:inch_ex, "~> 2.1.0-rc.1", only: :docs},
+      # github.com/dwyl/learn-pre-commit
+      {:pre_commit, "~> 0.3.4", only: :dev}
     ]
   end
 
