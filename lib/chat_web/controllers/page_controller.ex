d4fe4fc747fd47d4092a07e600d5bdd85ebb4880
nelsonic <nelson+github@dwyl.com>
2022-11-14 17:24:03 +0000

    comment out unused code until tests are written for it ... #54

--- lib/chat_web/controllers/page_controller.ex
+++ lib/chat_web/controllers/page_controller.ex
@@ -5,9 +5,9 @@ defmodule ChatWeb.PageController do
     render(conn, "index.html")
   end
 
-  def auth_page(conn, _params) do
-    render(conn, "auth.html")
-  end
+  # def auth_page(conn, _params) do
+  #   render(conn, "auth.html")
+  # end
 
   # see: github.com/dwyl/ping
   def ping(conn, params) do
