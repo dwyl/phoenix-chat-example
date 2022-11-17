1b18ff328a9dacb7ec1694cca41a824ccdf0a052
nelsonic <nelson+github@dwyl.com>
2022-11-17 13:30:39 +0000

    create auth_controller.ex for #54

--- lib/chat_web/controllers/page_controller.ex
+++ lib/chat_web/controllers/page_controller.ex
@@ -4,13 +4,4 @@ defmodule ChatWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
-
-  # def auth_page(conn, _params) do
-  #   render(conn, "auth.html")
-  # end
-
-  # see: github.com/dwyl/ping
-  def ping(conn, params) do
-    Ping.render_pixel(conn, params)
-  end
 end
