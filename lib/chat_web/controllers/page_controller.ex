f4a6f14c7ff55f9eda96e15108dbcedc7a6a17e1
Luís Arteiro <lucho14garfield@gmail.com>
2022-11-23 13:48:14 +0000

    fix: Removing unnecessary dbs. #121

--- lib/chat_web/controllers/page_controller.ex
+++ lib/chat_web/controllers/page_controller.ex
@@ -4,6 +4,5 @@ defmodule ChatWeb.PageController do
   def index(conn, _params) do
     messages = Chat.Message.get_messages
     render(conn, "index.html", messages: messages)
-    |> dbg()
   end
 end
--- lib/chat_web/views/page_view.ex
+++ lib/chat_web/views/page_view.ex
@@ -2,7 +2,6 @@ defmodule ChatWeb.PageView do
   use ChatWeb, :view
 
   def person_name(person) do
-    dbg(person)
     person.givenName || person.username || "guest"
   end
 end
