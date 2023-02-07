596ca4e64cbc840d7c66d6408ccad04b4a5b13d8
Nelson <194400+nelsonic@users.noreply.github.com>
2023-02-07 13:53:10 +0000

    restore aliases in mix.exs see: https://github.com/dwyl/phoenix-chat-example/pull/152#pullrequestreview-1286927356

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chat.MixProject do
   def project do
     [
       app: :chat,
-      version: "0.1.0",
+      version: "1.17.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -12,10 +12,13 @@ defmodule Chat.MixProject do
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
+        c: :test,
         coveralls: :test,
         "coveralls.detail": :test,
+        "coveralls.json": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        t: :test
       ]
     ]
   end
@@ -56,8 +59,9 @@ defmodule Chat.MixProject do
       {:plug_cowboy, "~> 2.5"},
 
       # Testing
-      {:excoveralls, "~> 0.15.2", only: [:test, :dev]}, # tracking test coverage
-      {:auth_plug, "~> 1.5"},
+      # tracking test coverage
+      {:excoveralls, "~> 0.15.2", only: [:test, :dev]},
+      {:auth_plug, "~> 1.5"}
     ]
   end
 
@@ -74,7 +78,9 @@ defmodule Chat.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      c: ["coveralls.html"],
+      t: ["test"]
     ]
   end
 end
