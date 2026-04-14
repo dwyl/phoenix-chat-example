77e3c1cc0fcf9b2436904c132c802bf3bd5770d1
n <nelson+github@dwyl.com>
2026-04-14 23:57:16 +0100

    git rm -rf "old files" #330

--- test/chat_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ChatWeb.PageControllerTest do
-  use ChatWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Phoenix Chat Example"
-  end
-end
